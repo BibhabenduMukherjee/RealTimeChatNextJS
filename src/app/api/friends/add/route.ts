f27e94f8d72a09666749ed993a2d5293c413b4b7
= <mukherjee4004@gmail.com>
2023-04-18 20:42:33 +0530

    implement addFriend route with secure session taking care of

--- src/app/api/friends/add/route.ts
+++ src/app/api/friends/add/route.ts
@@ -1,10 +1,30 @@
+import { authOptions } from "@/lib/auth";
 import { addFriendValidate } from "@/lib/validation/add-friend";
+import { getServerSession } from "next-auth";
 
 export async function POST(req:Request){
 try{
     const body = await req.json();
     const {email: emailToAdd } = addFriendValidate.parse(body.email);
-
-
+    const RESTRespose = await fetch(`
+    ${process.env.UPSTASH_REDIS_REST_URL}/get/user:email${emailToAdd}` , {
+        headers : {
+            Authorization : `Bearer ${process.env.UPSTASH_REDIS_REST_TOKEN}`
+        },
+        cache : 'no-store'
+    })
+    const data =await RESTRespose.json() as {result:string};
+    const idToAdd = data.result;
+    if(!idToAdd){
+        return new Response('Person does not presnt' , {status : 401})
+    }
+    const session = await getServerSession(authOptions)
+    if(idToAdd === session?.user.id){
+        return new Response('Cannot not add' , {status : 401})
+    }
+   
+    if(!session){
+        return new Response('Unauthorized' , {status : 404})
+    }    
 }catch(err){}
 }
