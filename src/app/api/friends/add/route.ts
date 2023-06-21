756674389c0705a61fa021910edcd4d9260153cc
= <mukherjee4004@gmail.com>
2023-06-21 17:37:40 +0530

    add middleware to the sensitive paths

--- src/app/api/friends/add/route.ts
+++ src/app/api/friends/add/route.ts
@@ -1,56 +1,82 @@
-import { fetchRedis } from "@/helper/redis";
-import { authOptions } from "@/lib/auth";
-import { db } from "@/lib/db";
-import { addFriendValidate } from "@/lib/validation/add-friend";
-import { getServerSession } from "next-auth";
-import {z}  from "zod"
-export async function POST(req:Request){
-try{
+
+import { fetchRedis } from '@/helper/redis'
+import { authOptions } from '@/lib/auth'
+import { db } from '@/lib/db'
+
+import { toPusherKey } from '@/lib/utils'
+import { addFriendValidate } from '@/lib/validation/add-friend'
+
+import { getServerSession } from 'next-auth'
+import { z } from 'zod'
+
+export async function POST(req: Request) {
+  try {
+    const body = await req.json()
+
+    const { email: emailToAdd } = addFriendValidate.parse(body.email)
+
+    const idToAdd = (await fetchRedis(
+      'get',
+      `user:email:${emailToAdd}`
+    )) as string
+
+    if (!idToAdd) {
+      return new Response('This person does not exist.', { status: 400 })
+    }
+
     const session = await getServerSession(authOptions)
-    if(!session){
-        return new Response('Unauthorized' , {status : 404})
-    }  
 
-   
-
-    const body = await req.json();
-    const {email: emailToAdd } = addFriendValidate.parse(body.email);
-    //console.log(emailToAdd);
-    if(emailToAdd === session?.user.email){
-        return new Response('Cannot not add yourself' , {status : 401})
-    }
-    const idToAdd = await fetchRedis('get' , `user:email:${emailToAdd}`) as string
-    //console.log(idToAdd)
-    if(!idToAdd){
-
-        // as status 400 series consider as a AxiosError class
-        return new Response('Person does not presnt' , {status : 401})
-    }
-   
-   
-    // send the requested user id and current_user to check ismember 
-    const isAlreadyExist = ( await fetchRedis('sismember' , `user:${idToAdd}:incoming_friend_requests` , session.user.id) ) 
-    if(isAlreadyExist){
-        return new Response('Request is already sent' , {status: 400})
+    if (!session) {
+      return new Response('Unauthorized', { status: 401 })
     }
 
-
-    const isAlreadyFriend = ( await fetchRedis('sismember' , `user:${session.user.id}:friends` , idToAdd) ) 
-    if(isAlreadyFriend){
-        return new Response('This user is Already added as a Friend' , {status: 400})
+    if (idToAdd === session.user.id) {
+      return new Response('You cannot add yourself as a friend', {
+        status: 400,
+      })
     }
 
-    db.sadd(`user:${idToAdd}:incoming_friend_requests` , session.user.id)
-    return new Response('ok')
-     
-}catch(err){
-    if(err instanceof z.ZodError){
-        return new Response('invalid request payload' , {status : 422})
+    // check if user is already added
+    const isAlreadyAdded = (await fetchRedis(
+      'sismember',
+      `user:${idToAdd}:incoming_friend_requests`,
+      session.user.id
+    )) 
+
+    if (isAlreadyAdded) {
+      return new Response('Already added this user', { status: 400 })
     }
+
+    // check if user is already added
+    const isAlreadyFriends = (await fetchRedis(
+      'sismember',
+      `user:${session.user.id}:friends`,
+      idToAdd
+    )) 
+
+    if (isAlreadyFriends) {
+      return new Response('Already friends with this user', { status: 400 })
+    }
+
+    // valid request, send friend request
+
+    // await pusherServer.trigger(
+    //   toPusherKey(`user:${idToAdd}:incoming_friend_requests`),
+    //   'incoming_friend_requests',
+    //   {
+    //     senderId: session.user.id,
+    //     senderEmail: session.user.email,
+    //   }
+    // )
+
+    await db.sadd(`user:${idToAdd}:incoming_friend_requests`, session.user.id)
+
+    return new Response('OK')
+  } catch (error) {
+    if (error instanceof z.ZodError) {
+      return new Response('Invalid request payload', { status: 422 })
+    }
+
+    return new Response('Invalid request', { status: 400 })
+  }
 }
-
-return new Response('invalid request' , {status :400})
-}
-
-
-//
--- src/components/FriendRequests.tsx
+++ src/components/FriendRequests.tsx
@@ -1,5 +1,7 @@
 'use client'
+import axios from "axios"
 import { Check, UserPlus, X } from "lucide-react"
+import { useRouter } from "next/navigation"
 import {FC, useState} from "react"
 interface FriendRequestsProrps{
     incomingFriendRequest:IncomingFriendRequest[]
@@ -7,9 +9,32 @@ interface FriendRequestsProrps{
 }
 
 const FriendRequests:FC<FriendRequestsProrps> = ({incomingFriendRequest , sessionId})=>{
+    const router = useRouter();
     const [friendRequests , setFriendRequests] = useState<IncomingFriendRequest[]>(
         incomingFriendRequest
     )
+    // if we have 3 friend request  a,b,c and we accept
+    // b then we need to take out b from state
+    const acceptFriend = async (senderId:string) =>{
+        await axios.post('/api/friends/accept' , {id : senderId})
+        setFriendRequests((prev)=>
+            prev.filter((request)=> request.senderId !== senderId)
+        )
+        router.refresh();
+
+
+    }
+
+    const denyFriend = async (senderId:string) =>{
+        await axios.post('/api/friends/deny' , {id : senderId})
+        setFriendRequests((prev)=>
+            prev.filter((request)=> request.senderId !== senderId)
+        )
+        router.refresh();
+
+
+    }
+
     return (
         <>
         {friendRequests.length === 0 ? (<>
@@ -19,14 +44,14 @@ const FriendRequests:FC<FriendRequestsProrps> = ({incomingFriendRequest , sessio
         <div key={r.senderId} className="ml-4 flex space-x-2 gap-4 items-center">
             <UserPlus className="text-black"/>
             <p className="font-medium text-lg">{r.senderEmail}</p>
-            <button aria-label="accept friend request" 
+            <button aria-label="accept friend request"  onClick={()=>{acceptFriend(r.senderId)}}
             className="w-8 h-8 bg-indigo-600 hover:bg-indigo-700 grid place-items-center rounded-full transition hover:shadow-md"
             ><Check className="font-semibold text-white w-3/4 h-3/4"/></button>
 
-            <button aria-label="accept friend request" 
+            <button onClick={()=> denyFriend(r.senderId)} aria-label="accept friend request" 
             className="w-8 h-8 bg-red-600 hover:bg-red-700 grid place-items-center rounded-full transition hover:shadow-md"
             ><X className="font-semibold text-white w-3/4 h-3/4"/></button>
-
+ 
         </div>
        ))}
         </>)}
